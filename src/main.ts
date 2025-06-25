dd6c56d51eb18209557ba5d443a9cd08e595da2a
Vo Thong Khoa <khoaxilo951@gmail.com>
2025-06-25 10:05:40 +0700

    update enable cors using .env

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   );
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get<string>('FE_HOST'),
     credentials: true,
   });
 
