409ecff06ca09dc8573695423bb6129556438217
thongkhoav <thongkhoa2002@gmail.com>
2025-08-27 22:55:56 +0700

    add typeorm db migration

---
+++ data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+// Load .env file before anything else
+dotenv.config();
+console.log('DB_PORT', process.env.DB_PORT);
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+});
