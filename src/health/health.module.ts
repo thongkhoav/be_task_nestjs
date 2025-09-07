65896424b595a5edbfe97f6e875aa27d7d9e3a6b
thongkhoav <thongkhoa2002@gmail.com>
2025-09-07 12:40:52 +0700

    fiux health module

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 
 @Module({
   controllers: [HealthController],
-  providers: [HealthService],
 })
 export class HealthModule {}
