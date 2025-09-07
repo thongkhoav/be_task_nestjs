93144e0ceff8800c2721d558cc3444ff1359983e
thongkhoav <thongkhoa2002@gmail.com>
2025-09-07 16:36:46 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   );
   app.use(cookieParser());
   app.enableCors({
-    origin: '*',
+    origin: ['localhost:3000', process.env.FE_HOST],
     credentials: true,
   });
 
